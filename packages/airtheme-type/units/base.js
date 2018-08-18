971d5cd33794eafa1b5ec8fd4178dd198bae34a4
ksure <kyle.scherling@gmail.com>
2018-08-17 21:34:56 -0700

    Adds unit base schema and factory methods

---
+++ packages/airtheme-type/units/base.js
@@ -0,0 +1,28 @@
+// unit
+// The base unit for all css units
+
+export const unit = {
+
+    // type
+    // string : The name of the unit.
+
+    type: 'unit',
+
+    // value
+    // Any : The value of the unit
+
+    value: null
+}
+
+// createUnit
+// Accepts a type and returns a unit
+
+const createUnit = ({ type }) =>  Object.assign({}, unit, { type })
+
+// createFactory
+// Accepts a unit object and returns a function that is used to create the unit
+
+const createUnitFactoryFor = unit => ({ value }) => Object.assign({}, unit, { value })
+
+
+export default createUnit
