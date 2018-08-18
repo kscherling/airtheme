4d70b05494b07e9eb4aea20fcc020879a7bb823e
ksure <kyle.scherling@gmail.com>
2018-08-17 21:34:27 -0700

    Adds types base schema

---
+++ packages/airtheme-type/types/base.js
@@ -0,0 +1,32 @@
+const type = {
+
+    // type
+    // The name of the type. The same as the 'class'
+
+    type: 'type',
+
+    // display
+    // The unit the content items are displayed. User selected.
+
+    display: '',
+
+    // unit
+    // The unit the content items are stored. Static.
+
+    unit: '',
+
+    // options
+    // Array : Units this type can serialize to.
+
+    options: [],
+
+    // content
+    // Object : key/val store for settings. User selected.
+
+    content: {}
+  }
+
+
+// hexa
+// A made up unit (hex, alpha) can express any hex, rgb, or rgba value.
+// Units are serialized to the smallest css var that expresses
