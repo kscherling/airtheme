76772fcb131bbb7e65aac2e96b2834c2d228c94c
ksure <kyle.scherling@gmail.com>
2018-08-21 00:27:06 -0700

    Adds action name contants

---
+++ packages/airtheme-make/actions.js
@@ -0,0 +1,33 @@
+export const UPDATE_THEME = 'updateTheme'
+
+export const UPDATE_BASE_FONT_SIZE = 'updateBaseFontSize'
+export const UPDATE_BASE_LINE_HEIGHT = 'updateBaseLineHeight'
+export const UPDATE_BASE_SPACING = 'updateBaseSpacing'
+
+export const UPDATE_COLOR = 'updateColor'
+export const ADD_COLOR = 'addColor'
+export const REMOVE_COLOR = 'removeColor'
+
+export const UPDATE_FONT_SIZE = 'updateFontSize'
+export const ADD_FONT_SIZE = 'addFontSize'
+export const REMOVE_FONT_SIZE = 'removeFontSize'
+
+export const UPDATE_FONT_WEIGHT = 'updateFontWeight'
+export const ADD_FONT_WEIGHT = 'addFontWeight'
+export const REMOVE_FONT_WEIGHT = 'removeFontWeight'
+
+export const UPDATE_LINE_HEIGHT = 'updateLineHeight'
+export const ADD_LINE_HEIGHT = 'addLineHeight'
+export const REMOVE_LINE_HEIGHT = 'removeLineHeight'
+
+export const UPDATE_FONT_FAMILY = 'updateFontFamily'
+export const ADD_FONT_FAMILY = 'addFontFamily'
+export const REMOVE_FONT_FAMILY = 'removeFontFamily'
+
+export const UPDATE_SPACING = 'updateSpacing'
+export const ADD_SPACING = 'addSpacing'
+export const REMOVE_SPACING = 'removeSpacing'
+
+export const ADD_SWATCH = 'addSwatch'
+export const UPDATE_SWATCH = 'updateSwatch'
+export const REMOVE_SWATCH = 'removeSwatch'
