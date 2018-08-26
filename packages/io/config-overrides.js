c9bb39f995bb7e1fc77f1efdb7de8d7dab39f126
ksure <kyle.scherling@gmail.com>
2018-08-26 01:08:51 -0700

    Alias share modules

--- packages/io/config-overrides.js
+++ packages/io/config-overrides.js
@@ -1,7 +1,10 @@
 module.exports = function override(config, env) {
   config.resolve = {
     alias: {
-      'styled-components': require.resolve('styled-components')
+      'styled-components': require.resolve('styled-components'),
+      react: require.resolve('react'),
+      redux: require.resolve('redux'),
+      'react-redux': require.resolve('react-redux')
     }
   }
   return config
