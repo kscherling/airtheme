0f7d4fd9c8c4c9536bf9abfe9f402484c62c7ed5
ksure <kyle.scherling@gmail.com>
2018-08-28 00:11:00 -0700

    update make

--- packages/make/config-overrides.js
+++
@@ -1,8 +0,0 @@
-module.exports = function override(config, env) {
-  config.resolve = {
-    alias: {
-      'react-redux': require.resolve('react-redux')
-    }
-  }
-  return config
-}
--- packages/make/rollup.config.js
+++ packages/make/rollup.config.js
@@ -1,19 +1,11 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
+const baseConfig = require('@airtheme/shared/rollup.config.js')
 
-export default {
-  input: 'index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs'
-  },
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    commonjs()
-  ],
-  external: ['@airtheme/type', '@airtheme/core', 'react-redux']
+module.exports = {
+  ...baseConfig,
+  external: [
+    ...baseConfig.external,
+    '@airtheme/type',
+    '@airtheme/core',
+    'react-redux'
+  ]
 }
