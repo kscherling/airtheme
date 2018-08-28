4ad7b5662e45c088f50f61da91437767496fb5ef
ksure <kyle.scherling@gmail.com>
2018-08-27 23:32:14 -0700

    Adds share rollup config and deps

---
+++ packages/shared/rollup.config.js
@@ -0,0 +1,18 @@
+const babel = require('rollup-plugin-babel')
+const commonjs = require('rollup-plugin-commonjs')
+const resolve =  require('rollup-plugin-node-resolve')
+
+modules.export = {
+  input: 'index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs'
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    commonjs()
+  ]
+}
