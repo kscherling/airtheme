2961c07cf029a4172a1efb79064120708125cb12
ksure <kyle.scherling@gmail.com>
2018-08-29 22:01:16 -0700

    Closer to getting babel7 updated

--- packages/shared/babel.config.js
+++ packages/shared/babel.config.js
@@ -1,18 +1,16 @@
 module.exports = {
+  presets: [
+    ['@babel/preset-env', { modules: false }],
+    '@babel/preset-react'
+  ],
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties'
   ],
   env: {
-    build: {
-      presets: [
-        ['@babel/preset-env', { modules: false }],
-        '@babel/preset-react'
-      ],
-    },
     test: {
       presets: [
-        ['@babel/preset-env'], { modules: 'cjs' },
+        ['@babel/preset-env'], { modules: 'commonjs' },
         '@babel/preset-react'
       ]
     }
