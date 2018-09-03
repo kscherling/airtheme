2bf9aaf1c0a52c051fc2a18eb127bc91558ea770
ksure <kyle.scherling@gmail.com>
2018-09-03 16:58:55 -0700

    Adds globals to theme export

--- packages/assemble/src/object/setting.js
+++ packages/assemble/src/object/setting.js
@@ -4,20 +4,29 @@ import {
   BASE_LINE_HEIGHT_KEY,
   BASE_SPACING_KEY,
   COLOR_KEY,
+  FONT_FACE_KEY,
   FONT_FAMILY_KEY,
   FONT_SIZE_KEY,
   FONT_WEIGHT_KEY,
   SETTING_KEY,
-  SPACING_KEY
+  SPACING_KEY,
+  SWATCH_KEY
 } from '@airtheme/core'
 
 const setting = (next, input, theme) => {
   return next(
     {
       ...input,
-      ...deserializeAttribute(theme.base.baseFontSize, theme),
-      ...deserializeAttribute(theme.base.baseSpacing, theme),
-      ...deserializeAttribute(theme.base.baseLineHeight, theme),
+      [BASE_FONT_SIZE_KEY]: deserializeAttribute(theme.base.baseFontSize, theme)
+        .baseFontSize,
+      [BASE_SPACING_KEY]: deserializeAttribute(theme.base.baseSpacing, theme)
+        .baseSpacing,
+      [BASE_LINE_HEIGHT_KEY]: deserializeAttribute(
+        theme.base.baseLineHeight,
+        theme
+      ).baseLineHeight,
+      [FONT_FACE_KEY]: deserializeAttribute(theme.fontFace, theme),
+      [SWATCH_KEY]: deserializeAttribute(theme.swatch, theme),
       [SETTING_KEY]: {
         [COLOR_KEY]: deserializeAttribute(theme.setting.color, theme),
         [FONT_FAMILY_KEY]: deserializeAttribute(
