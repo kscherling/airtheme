3d00ebfc7e3a2d8ddfb752977ea8737afe484d90
ksure <kyle.scherling@gmail.com>
2018-10-20 14:12:23 -0700

    Adds theme schema type

--- packages/type/src/theme/index.js
+++ packages/type/src/theme/index.js
@@ -1,8 +1,19 @@
+// @flow
 import schema from './schema'
 import base from './types/base'
 import factoryFor from './factoryFor'
 
-const theme = {
+type Theme = {
+  object: string,
+  name: string,
+  version: number
+}
+
+type ThemeTypes = {
+  base: Theme
+}
+
+const theme: ThemeTypes = {
   base: factoryFor(base, schema)
 }
 
--- packages/type/src/theme/schema.js
+++ packages/type/src/theme/schema.js
@@ -1,50 +1,34 @@
+// @flow
 import { _ROOT_THEME_ } from '@airtheme/core'
 import { VERSION, uuid } from '@airtheme/core'
 import attribute from '../attribute'
 import setting from '../setting'
 import base from '../base'
 
-// theme
-// The container object
+/**
+ *  Schema for theme object.
+ */
 
-export default {
-  // name
-  // Friendly name of the theme
+export type Schema = {
+  base: any,
+  fontFace: any,
+  id: string,
+  name: string,
+  object: string,
+  setting: any,
+  swatch: any,
+  version: string
+}
 
-  name: null,
-
-  // id
-  // you know, the id
-
+const schema: Schema = {
+  name: 'Default',
   id: uuid(),
-
-  // object
-  // string : The name of the unit.
-
   object: _ROOT_THEME_,
-
-  // version
-  // string : the version of the airthemek
-
   version: VERSION,
-
-  // base
-  // Base : Keyed object of base attribute types
-
   base: base.base(),
-
-  // swatch
-  // Swatch : Swatch attribute object
-
   swatch: attribute.swatch(),
-
-  // fontFace
-  // FontFace : fontFace attribute object
-
   fontFace: attribute.fontFace(),
-
-  // setting
-  // Setting : Keyed object of attribute types
-
   setting: setting.base()
 }
+
+export default schema
